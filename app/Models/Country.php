9527b388ec421d2ccccceaab4a2dfa3ee5fd0a87
Rui <rui95fer@gmail.com>
2024-12-28 15:19:20 +0000

    feat(models): Define relationships between Country and User models

---
+++ app/Models/Country.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Country extends Model
+{
+    protected $fillable = [
+        'name',
+        'is_active',
+    ];
+
+    /**
+     * Get the users associated with the country.
+     *
+     * @return HasMany
+     */
+    public function users(): HasMany
+    {
+        return $this->hasMany(User::class);
+    }
+}
